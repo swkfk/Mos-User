0942ab12f964fbffbd2086518fca58bee496e549
kai_Ker <kai_Ker@buaa.edu.cn>
2024-06-23 00:34:50 +0800

    [Mk]   Add the release profile for user apps

--- include.mk
+++ include.mk
@@ -5,7 +5,14 @@ CROSS_COMPILE  := mips-linux-gnu-
 CC             := $(CROSS_COMPILE)gcc
 CFLAGS         += --std=gnu99 -$(ENDIAN) -G 0 -mno-abicalls -fno-pic \
                   -ffreestanding -fno-stack-protector -fno-builtin \
-                  -Wa,-xgot -Wall -mxgot -mno-fix-r4000 -march=4kc
+                  -Wa,-xgot -Wall -mxgot -mno-fix-r4000 -march=24kc
 LD             := $(CROSS_COMPILE)ld
 LDFLAGS        += -$(ENDIAN) -G 0 -static -n -nostdlib --fatal-warnings
 
+ifneq ($(MOS_RELEASE),)
+	CFLAGS   := $(CFLAGS) -O2
+	LDFLAGS  := $(LDFLAGS) -O --gc-sections
+else
+	CFLAGS   := $(CFLAGS) -O0 -g -ggdb -DMOS_DEBUG
+    LDFLAGS  := $(LDFLAGS)
+endif
